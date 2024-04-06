9483964031bf1e89117590d057b1357db7d062d7
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-04-06 13:00:16 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/error_page/error_page_widget.dart
+++ lib/pages/error_page/error_page_widget.dart
@@ -68,7 +68,7 @@ class _ErrorPageWidgetState extends State<ErrorPageWidget> {
               ),
               FFButtonWidget(
                 onPressed: () async {
-                  context.pushNamed('authenticate');
+                  context.safePop();
                 },
                 text: 'Go Back',
                 options: FFButtonOptions(
